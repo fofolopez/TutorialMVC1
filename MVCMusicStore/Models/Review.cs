97722f2f4d1cec55b46c5b026e6e605aae4c50a3
Adolfo Lopez <fofolopez@outlook.com>
2017-04-02 23:25:49 -0600

    Tutorial segunda parte

---
+++ MVCMusicStore/Models/Review.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCMusicStore.Models
+{
+    public class Review
+    {
+        public int ReviewID { get; set; }
+
+        public string Contents { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string ReviewerEmail { get; set; }
+
+        public virtual Album Album { get; set; }
+    }
+
+}
